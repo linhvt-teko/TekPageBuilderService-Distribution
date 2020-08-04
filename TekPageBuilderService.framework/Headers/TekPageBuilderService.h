8fdbb9b2633793ce5494f7fbde728ef51ccd2772
linhvt <linh.vt@teko.vn>
2020-08-04 15:55:07 +0700

    - minor

---
+++ TekPageBuilderService.framework/Headers/TekPageBuilderService.h
@@ -0,0 +1,19 @@
+//
+//  TekPageBuilderService.h
+//  TekPageBuilderService
+//
+//  Created by linhvt on 8/4/20.
+//  Copyright © 2020 Deli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TekPageBuilderService.
+FOUNDATION_EXPORT double TekPageBuilderServiceVersionNumber;
+
+//! Project version string for TekPageBuilderService.
+FOUNDATION_EXPORT const unsigned char TekPageBuilderServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TekPageBuilderService/PublicHeader.h>
+
+
